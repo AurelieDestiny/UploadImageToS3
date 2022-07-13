5b0e2b9834695bf901c8b4491a0912225e489dba
Destynee <aurlie.martin@gmail.com>
2022-07-13 14:30:05 -0400

    Update HelloWorld.vue
    Update Api endpoint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://ylnt7vguu0.execute-api.us-east-2.amazonaws.com/default/mys3uploaderf1'
+const API_ENDPOINT = 'https://8duwtnz0k1.execute-api.us-east-1.amazonaws.com/default/myfirstserverless'
 
 export default {
   name: 'app',
